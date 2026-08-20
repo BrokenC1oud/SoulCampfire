2e62898a91dfde7da9b9a653a29ee498787023ce
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-08-21 07:36:24 +0800

    zig automatic detect memory leaks using init's gpa

--- src/main.zig
+++ src/main.zig
@@ -6,20 +6,13 @@ const Zenver = @import("zenver").Zenver;
 const SoulCampfire = @import("SoulCampfire");
 
 pub fn main(init: std.process.Init) !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer {
-        const deinit_status = gpa.deinit();
-        if (deinit_status == .leak) @panic("Memory leaked");
-    }
-
-    var zenv = Zenver.init(allocator, init.io, ".env");
+    var zenv = Zenver.init(init.gpa, init.io, ".env");
     defer zenv.deinit();
 
     try zenv.loadFile(null);
 
     var game: SoulCampfire.game.Game = undefined;
-    try game.init(allocator, init.io, &zenv);
+    try game.init(init.gpa, init.io, &zenv);
     defer game.deinit();
 
     game.registerSignal();
