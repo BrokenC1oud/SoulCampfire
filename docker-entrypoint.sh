c71ffeeb282ac8e05d31060740c4147e0daf7fa1
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-08-23 17:41:42 +0800

    shit

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# A bind mount replaces the image directory and may be owned by root.
-mkdir -p /app/data
-chown soulcampfire:soulcampfire /app/data
-
-exec gosu soulcampfire:soulcampfire /app/SoulCampfire "$@"
